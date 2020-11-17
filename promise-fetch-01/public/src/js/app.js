4ab938a3198672c4f6dd891037b2035ecaae004c
Hassan Ali <prog.hasan@gmail.com>
2020-11-17 10:30:44 +0300

    Applied sync Added Promise and fetch for old prowsers

--- promise-fetch-01/public/src/js/app.js
+++ promise-fetch-01/public/src/js/app.js
@@ -1,3 +1,7 @@
+if (!window.Promise) {
+    window.Promise = Promise;
+}
+
 var deferredPrompt;
 
 if ('serviceWorker' in navigator) {
@@ -15,8 +19,32 @@ window.addEventListener('beforeinstallprompt', (e) => {
     return false;
 });
 
-setTimeout(() => {
-    console.log('This is executed once the timer is done!');
-}, 3000);
+var promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        // resolve('This is executed once the timer is done!');
+        reject({
+            code: 500,
+            message: 'Oh no.. an error occured!'
+        });
+    }, 3000);
+});
+
+fetch('http://httpbin.org/ip')
+    .then(res => res.json())
+    .then(data => console.info('IP : ' + data.origin))
+    .catch(err => console.error(err));
+
+fetch('http://httpbin.org/post', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({ message: 'Does this work?' })
+    }).then(res => res.json())
+    .then(data => console.info(data.json.message))
+    .catch(err => console.error(err));
+
+promise.then(r => console.log(r)).catch(er => console.error("You've got an error", er));
 
 console.log('This is executed right after setTimeout()');
