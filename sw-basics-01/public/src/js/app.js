856ee7a8a22ec8889209e67180ac24e190031a98
Hassan Ali <prog.hasan@gmail.com>
2020-11-17 09:14:59 +0300

    Excercise finished

--- sw-basics-01/public/src/js/app.js
+++ sw-basics-01/public/src/js/app.js
@@ -1,5 +1,14 @@
-if('serviceWorker' in navigator) {
+var deferredPrompt;
+
+if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(() => {
         console.log('Service worker registered.');
     });
 }
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    console.info('beforeinstallprompt Fired');
+    e.preventDefault();
+    deferredPrompt = e;
+    return false;
+});
--- sw-basics-01/public/sw.js
+++ sw-basics-01/public/sw.js
@@ -1,8 +1,15 @@
 self.addEventListener('install', (e) => {
-    console.log('[ServiceWorker] Installing serviceWorker ...', e);
+    console.log('[ServiceWorker] Installing serviceWorker ...');
 });
 
 self.addEventListener('activate', (e) => {
-    console.log('[ServiceWorker] Activating serviceWorker ...', e);
+    console.log('[ServiceWorker] Activating serviceWorker ...');
     return self.clients.claim();
 });
+
+// self.addEventListener('fetch', (e) => {
+//     console.log('[ServiceWorker] Fetching content ...', e);
+//     e.respondWith(fetch(e.request));
+// });
+
+//
